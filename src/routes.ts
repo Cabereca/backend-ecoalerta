91cc4ea3948e44771cc4a5e41c257b1838ae3b11
ndbzika <chakcpps@gmail.com>
2023-12-14 19:15:47 -0300

    :bug: fix: User routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,13 +1,9 @@
 /* eslint-disable @typescript-eslint/no-misused-promises */
 import { Router } from 'express';
-import { userController } from './controllers/userController';
+import userRoutes from './routes/userRoutes';
 
 const routes = Router();
 
-routes.get('/users', userController.findAllUsers);
-routes.get('/users/:id', userController.findUser);
-routes.post('/users', userController.createUser);
-routes.put('/users/:id', userController.updateUser);
-routes.delete('/users/:id', userController.deleteUser);
+routes.use('/users', userRoutes);
 
 export default routes;
---
+++ src/routes/userRoutes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { userController } from '../controllers/userController';
+
+const routes = Router();
+
+routes.get('/users', userController.findAllUsers);
+routes.get('/users/:id', userController.findUser);
+routes.post('/users', userController.createUser);
+routes.put('/users/:id', userController.updateUser);
+routes.delete('/users/:id', userController.deleteUser);
+
+export default routes;
